fd8d0ca446420a4bf87f5f20073ae554919cace2
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-02-18 10:23:26 +0530

    389. Find the Difference

--- 389-Find the Difference.cpp
+++ 389-Find the Difference.cpp
@@ -44,3 +44,19 @@ public:
         return t[s.size()];
     }
 };
+
+// Runtime=10ms, Memory=6.8MB
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        for(int i=0;i<t.size();i++){
+            if(s[i]!=t[i])
+                return t[i];
+        }
+        return t[t.size()-1];
+    }
+};
