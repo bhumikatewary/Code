a0b16d0ec75f27a6b2c86cc71236b5a39b06f08a
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-03-11 11:23:01 +0530

    70. Climbing Stairs

--- 70-Climbing Stairs.cpp
+++ 70-Climbing Stairs.cpp
@@ -10,3 +10,24 @@ public:
         return dp[n];
     }
 };
+
+// Generalised Code
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int k=2; // 1.....n 
+        int dp[n+1];
+        dp[0]=1;
+        for(int i=1; i<=n; i++){
+           int ans=0;
+            for(int jump=1; jump<=k; jump++){
+               if(i-jump>=0){
+                ans+=dp[i-jump];
+               }
+            }
+            dp[i]=ans;
+        }
+        return dp[n];
+    }
+};
