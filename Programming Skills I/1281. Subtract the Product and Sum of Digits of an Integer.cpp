6d016503104aeee0aeaa0455e0c1fada9020a6b9
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-03-16 17:23:56 +0530

    1281. Subtract the Product and Sum

---
+++ Programming Skills I/1281. Subtract the Product and Sum of Digits of an Integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int digit, product=1, sum=0; 
+        while(n>0){
+            digit = n%10;
+            product = product * digit;
+            sum = sum + digit;
+            n = n/10;
+        }
+        return product - sum;
+    }
+};
