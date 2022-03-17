1bb351dfc945d44136cfd55ad9513a7ad04f68a1
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-03-17 21:57:01 +0530

    53. Maximum Subarray

---
+++ Data Structure I/53. Maximum Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      // Kadane's Algo
+         int temp=nums[0];
+        int ans=nums[0];
+        for(int i=1;i<nums.size();i++){
+            temp = max(temp+nums[i],nums[i]);
+            ans = max(ans,temp);
+        }
+        return ans;
+    }
+};
