c7d37fdc0c072547ff727c5f11ef1be8fde11a5e
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-04-04 23:03:30 +0530

    updated

--- Programming Skills I/1588. Sum of All Odd Length Subarrays.cpp
+++ Programming Skills I/1588. Sum of All Odd Length Subarrays.cpp
@@ -20,3 +20,20 @@ public:
     
     }
 };
+
+
+//commented 
+
+int sumOddLengthSubarrays(vector<int>& arr) {
+        int sum = 0;
+        int n = arr.size();
+        for(int i=0; i<n; ++i){
+            int start = n-i;          //calc. the subarrays start with the ith element & for every element that is: size-index
+            int end  = i+1;          // calc. the subarrays end with the ith element & that is: i+1
+            int total =  start*end;  // the total no of subarrays in which ith element is present are: start*end
+            int odd = total/2;       // usually half of total is odd
+            if(total%2 != 0)         // in case total is odd, then odd subarrays is one more than the even one
+                odd++;
+            sum+= odd*arr[i];        // for sum we multiply the no of times ith element present in odd subarrays with arr[i]
+        }
+        return sum;
