b07abe4c26f65f1186aa9c974ed795c5b15454f4
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-05-02 19:03:41 +0530

    Update new.cpp

--- May Challenge/new.cpp
+++ May Challenge/new.cpp
@@ -1,1 +1,24 @@
+// Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
 
+//Note that after backspacing an empty text, the text will continue empty.
+
+
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+       stack<char>s1,s2;
+		int n=s.size();
+		int m=t.size();
+		for(int i=0;i<n;i++)
+		{
+			if(s[i]=='#' and !s1.empty())s1.pop();
+			else if(s[i]!='#')s1.push(s[i]);
+		}
+		for(int i=0;i<m;i++)
+		{
+			if(t[i]=='#' and !s2.empty())s2.pop();
+			else if(t[i]!='#')s2.push(t[i]);
+		}
+		return s1==s2;
+    }
+};
