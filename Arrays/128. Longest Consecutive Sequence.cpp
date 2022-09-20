bf4d656362264a7b9570fa29ba3646449858f756
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-09-21 01:35:06 +0530

    Update 128. Longest Consecutive Sequence.cpp

--- Arrays/128. Longest Consecutive Sequence.cpp
+++ Arrays/128. Longest Consecutive Sequence.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+         int max_count=0,curr_count=0;
+	
+	// use unordered set and store all elem
+	unordered_set<int> s;
+	for(int i=0; i<nums.size();i++){
+		s.insert(nums[i]);
+	}
+	
+	// Traverse through the set
+	for(int i=0; i<nums.size();i++){
+		if(!s.count(nums[i]-1)){ 
+			curr_count = 1;
+			while(s.count(nums[i]+1)){ // check consecutive for each number
+				curr_count++;
+				nums[i]+=1;
+			}
+			max_count = max(max_count,curr_count); 
+		}
+	}
+	return max_count;
+    }
+};
