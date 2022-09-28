d80347db480589bf686166f16039c25fadbc2019
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-09-28 11:41:06 +0530

    Update and rename new1 to 90. Subsets II.cpp

--- Recursion/90. Subsets II.cpp
+++ Recursion/90. Subsets II.cpp
@@ -29,3 +29,27 @@ public:
          return ans;   
     }
 };
+
+
+
+// OR
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void fun(vector<int>& candidates, vector<int>&ds,int index){
+       ans.push_back(ds);
+      for(int i=index;i<candidates.size();i++){
+           if(i>index&&candidates[i]==candidates[i-1])continue;
+              ds.push_back(candidates[i]);
+              fun(candidates,ds,i+1);
+              ds.pop_back();
+      }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+      vector<int>ds;
+      sort(nums.begin(),nums.end());
+      fun(nums,ds,0);
+      return ans;
+    }
+};
