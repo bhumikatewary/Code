10b3dde9dff0c7c006ccc1bef7f8887b7048583e
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-09-28 11:39:17 +0530

    Update 78. Subsets.cpp

--- Recursion/78. Subsets.cpp
+++ Recursion/78. Subsets.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     
+     void subsets(vector<int>&nums,vector<int>output,int index,vector<vector<int>>&ans)
+    {
+        if(index==nums.size())
+        {              // only that values of output are stored in ans where index is >=nums size not all values will be added
+            ans.push_back(output);
+            return;
+        }
+        
+        //excludes elements of nums so no other value is added in output vector
+        subsets(nums,output,index+1,ans);
+        
+        //includes  elements of nums in output vector
+        output.push_back(nums[index]); //push the value of nums at index in output vecotor
+        subsets(nums,output,index+1,ans);
+        output.pop_back();
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+      vector<vector<int>> ans;
+         vector<int> output;
+         subsets(nums,output,0,ans);
+         return ans;   
+    }
+};
+
+
+
+//  OR
+
+
+class Solution {
+public:
+    
      vector<vector<int>> ans;
     void fun(vector<int>& candidates, vector<int>&ds,int index){
        ans.push_back(ds);
