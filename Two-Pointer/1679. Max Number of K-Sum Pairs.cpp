53dd26db9f82e84f487d6ed5bb1038c5b9c26a0b
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-12-04 14:41:29 +0530

    Create 1679. Max Number of K-Sum Pairs.cpp

---
+++ Two-Pointer/1679. Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+       //NlogN tc, two-pointer approach
+        
+        sort(nums.begin(),nums.end()); //first we sort the array
+        int count=0;
+        int i=0; int j=nums.size()-1;
+        while(i<j){
+            if(nums[i]+nums[j]==k){
+                i++;
+                j--;
+                count++;
+            }else if(nums[i]+nums[j]>k){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return count;
+    }
+};
